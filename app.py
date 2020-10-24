7c8de8c794530527c3f0dd157e385038dafa72fb
Aldayr Natal Filho <al.filho@aluno.faculdadeimpacta.com.br>
2020-10-24 13:07:29 -0300

    ac4

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def fib():
+	prox = 1
+   	ant = 0
+   	res = "0, "
+   	for i in range(50):
+       		tmp = prox
+       		prox = prox + ant
+       		ant = tmp
+		if i <= 49:
+       			res += str(prox) + ", "
+		else:
+			res += str(prox) 
+
+   	return res
+
+print(fib())
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
